d5f542605e1b8d855d1af413ccd9b25dcbc852a5
Michael Wurster <miwurster@googlemail.com>
2022-04-20 10:10:17 +0200

    feat: prepare automatic releases

--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
-import os
-
 from setuptools import setup, find_namespace_packages
 
-version = os.environ.get("VERSION", "1.0.1")
-
-with open("./README.md", "r") as fh:
+with open('./README.md', 'r') as fh:
     long_description = fh.read()
 
-with open("./requirements.txt", "r") as fh:
+with open('./requirements.txt', 'r') as fh:
     requirements = fh.readlines()
 
 setup(
-    name="planqk-quantum",
-    version=version,
-    author="StoneOne AG",
-    author_email="info@stoneone.de",
-    url="https://github.com/planqk/planqk-quantum",
-    description="Python library for the PlanQK Quantum Platform",
+    name='planqk-quantum',
+    version='1.0.1',
+    author='StoneOne AG',
+    author_email='info@stoneone.de',
+    url='https://github.com/planqk/planqk-quantum',
+    description='Python library for the PlanQK Quantum Platform',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=find_namespace_packages(include=["planqk", "planqk.*"]),
-    license="apache-2.0",
+    long_description_content_type='text/markdown',
+    packages=find_namespace_packages(include=['planqk', 'planqk.*']),
+    license='apache-2.0',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
     ],
     install_requires=requirements,
 )
