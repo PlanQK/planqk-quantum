6d031b9e2457041366541ade8bd156dadd4a8cf3
Michael Wurster <miwurster@googlemail.com>
2023-12-06 16:33:06 +0100

    chore: improve test

--- tests/integration/test_credentials.py
+++ tests/integration/test_credentials.py
@@ -1,7 +1,7 @@
 import os
+import tempfile
 import unittest.mock
 
-from planqk.context import get_config_file_path
 from planqk.qiskit import PlanqkQuantumProvider
 
 
@@ -10,6 +10,10 @@ class CredentialsTestSuite(unittest.TestCase):
     def setUp(self):
         if "SERVICE_EXECUTION_TOKEN" in os.environ:
             del os.environ["SERVICE_EXECUTION_TOKEN"]
+
+    def tearDown(self):
+        if "PLANQK_CONFIG_FILE_PATH" in os.environ:
+            del os.environ["PLANQK_CONFIG_FILE_PATH"]
 
     def test_should_use_user_provided_token(self):
         access_token = "user_access_token"
@@ -26,22 +30,20 @@ class CredentialsTestSuite(unittest.TestCase):
         self.assertEqual("user_access_token", planqk_provider.get_access_token())
 
     def test_should_get_access_token_from_config_file(self):
-        # check if config file exists, if not create it and write mock data
-        config_file_path = get_config_file_path()
-        if not os.path.isfile(config_file_path):
-            os.makedirs(os.path.dirname(config_file_path), exist_ok=True)
-            with open(config_file_path, 'w') as file:
-                file.write("""{
-                    "auth": {
-                        "type": "API_KEY", 
-                        "value": "plqk_test"
-                    }
-                }
-                """)
+        json_value = """
+        {
+            "auth": {
+                "type": "API_KEY", 
+                "value": "plqk_test"
+            }
+        }
+        """
+        with tempfile.NamedTemporaryFile(suffix=".json", delete=False) as fp:
+            fp.write(json_value.encode("utf-8"))
+            os.environ["PLANQK_CONFIG_FILE_PATH"] = os.path.abspath(fp.name)
 
         planqk_provider = PlanqkQuantumProvider()
 
         access_token = planqk_provider.get_access_token()
         self.assertIsNotNone(access_token)
-        self.assertIs(type(access_token), str)
-        self.assertIs(len(access_token) > 0, True)
+        self.assertEqual(access_token, "plqk_test")
