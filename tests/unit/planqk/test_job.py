cd86fe7fe4fc067b23cf6db6dcd433f571dbeedd
Michael Wurster <miwurster@googlemail.com>
2024-01-31 16:32:39 +0100

    chore: add 'TestBackend'

--- tests/unit/planqk/test_job.py
+++ tests/unit/planqk/test_job.py
@@ -1,15 +1,34 @@
 import unittest
+from abc import ABC
 
-from qiskit_aer import AerSimulator
+from qiskit.providers import BackendV2
 
 from planqk.qiskit import PlanqkJob
 from planqk.qiskit.client.job_dtos import JobDto
 
 
+class TestBackend(BackendV2, ABC):
+
+    @property
+    def target(self):
+        return None
+
+    @property
+    def max_circuits(self):
+        return None
+
+    @classmethod
+    def _default_options(cls):
+        pass
+
+    def run(self, run_input, **options):
+        pass
+
+
 class JobTestSuite(unittest.TestCase):
 
     def test_job_should_have_queue_position_attribute(self):
-        backend = AerSimulator()
+        backend = TestBackend()
         job = PlanqkJob(backend, job_id="1337", job_details=JobDto(**{"provider": "TEST"}))
 
         queue_position = job.queue_position()
